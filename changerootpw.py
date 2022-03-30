e8811ff4d7ac2faa607ee983aa91c7604831325e
Scott Mermelstein <scottmermelstein89@gmail.com>
2022-03-29 22:48:31 -0400

    Added in high score
    added ability to keep track of the high score

--- changerootpw.py
+++ changerootpw.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 import time
 import os
+
 start = time.time()
+name = "Scott"
+#check if the high score file exists
+if os.path.exists('score.txt'):
+    #check the previous high score
+    with open('score.txt', 'r') as file:
+        high_score = float(file.read())
+else:
+    high_score = 0
 
 def question(num, question, answer, hint):
     hint_text = "or 'h' for a hint"
@@ -17,86 +26,20 @@ def question(num, question, answer, hint):
             step = input("Step {}: {}:".format(num,question))
     print("Correct!")
 
-
 question(1,"Enter the character to press at grub", 'e', 'the letter e')
-# question(2,"What should be entered at the end of the 'linux16' line?", "rd.break", "'rd'")
-# question(3,"What is the command to remount the root file system as read/write?", "mount -o remount,rw /sysroot", "'mount -o remount'")
-# question(4,"Switch into a chroot jail?", "chroot /sysroot", "'sysroot")
-# question(5,"What is the step after chroot /sysroot?", "passwd", "'change the root password.'")
-# question(6,"Address all unlabeled files.", "touch /.autorelabel", "'touch ?autorelabel'")
-# question(7,"How do you get out and reboot back to the main system?", "exit", "'exit'")
+question(2,"What should be entered at the end of the 'linux16' line?", "rd.break", "'rd'")
+question(3,"What is the command to remount the root file system as read/write?", "mount -o remount,rw /sysroot", "'mount -o remount'")
+question(4,"Switch into a chroot jail?", "chroot /sysroot", "'sysroot")
+question(5,"What is the step after chroot /sysroot?", "passwd", "'change the root password.'")
+question(6,"Address all unlabeled files.", "touch /.autorelabel", "'touch ?autorelabel'")
+question(7,"How do you get out and reboot back to the main system?", "exit", "'exit'")
 
-
-# #Changing Root PW
-# ##step1
-# step1 = input("Step 1: Enter the character to press at grub or 'h' for a hint:")
-# while step1 != 'e':
-#   if ( step1 == 'h'):
-#     print("the letter 'e'")
-#     step1 = input("Step 1: Enter the character to press at grub:")
-#   elif (step1 == 'e'):
-#     print("Correct!")
-#   else:
-#     print("Fail")
-#     step1 = input("Step 1: Enter the character to press at grub:")
-
-
-# #Rd.break at the end of the linux16 line.
-# step2 = input("Step2 : What should be entered at the end of the 'linux16' line? Or 'h' for a hint.")
-
-# while step2 != 'rd.break':
-#   if ( step1 == 'h'):
-#     print("Hint: 'rd'")
-#     step2 = input("Step2 : What should be entered at the end of the 'linux16' line? ")
-#   elif (step1 == 'rd.break'):
-#     print("Correct!")
-#   else:
-#     print("Fail")
-#     step2 = input("Step2 : What should be entered at the end of the 'linux16' line? ")
-
-
-# #Mount -o remount,rw /sysroot
-
-# step3 = input("Step 3: What is the command to remount the root file system as read/write? Or 'h' for a hint.")
-# while step3 != "mount -o remount,rw /sysroot":
-#   if ( step3 == 'h'):
-#     print("Hint: 'mount -o remount'")
-#     step3 = input("Step 3: What is the command to remount the root file system as read/write?")
-#   elif (step3 == "mount -o remount,rw /sysroot"):
-#     print("Correct!")
-#   else:
-#     print("Fail")
-#     step3 = input("Step 3: What is the command to remount the root file system as read/write?")
- 
 # #Chroot /sysroot
 # #Passwd
 # #Enter password
 # #Repeat
 # #touch /.autorelabel
 # #Exit; exit
-
-# >>> hiscores = [(300, "John")]
-# >>> score = 200
-# >>> player = "Jane"
-# >>> hiscores.append((score, player))
-# >>> print hiscores
-# [(300, 'John'), (200, 'Jane')]
-# >>> hiscores.sort()
-# >>> print hiscores
-# [(200, 'Jane'), (300, 'John')]
-
-
-# >>> import pickle
-
-# >>> scores = open("scores", "w")
-# >>> pickle.dump(hiscores[-10:], scores)
-# >>> scores.close()
-
-# >>> scores = open("scores", "r")
-# >>> oldscores = pickle.load(scores)
-# >>> scores.close()
-# >>> print oldscores
-# [(200, 'Jane'), (300, 'John')]
 
 end = time.time()
 totalTime = end - start
@@ -108,25 +51,12 @@ else:
 
 print("Your score for 'Reset Root PW' is: ", score)
 
-# import pickle
-# #hiscores = [(end, "Scott")]
-# hiscores = []
-# # score = end
-# player = "Scott"
-# hiscores.append((score, player))
-# print(hiscores)
-# #[(300, 'John'), (200, 'Jane')]
-# #hiscores.sort()
-# #print hiscores
-# #[(200, 'Jane'), (300, 'John')]
-
-# scores = open("scores", "w")
-# pickle.dump(hiscores[-10:], scores)
-# scores.close()
-
-# scores = open("scores", "r")
-# oldscores = pickle.load(scores)
-# scores.close()
-# print(oldscores)
-# # [(200, 'Jane'), (300, 'John')]
-
+#Update high score
+if score > high_score:
+    high_score = score
+    with open('score.txt', 'w') as file:
+        file.write(str(high_score))
+    print("Congratulations! You go the new High Score!")
+    print("The New High Score is: {}".format(high_score))
+else:
+    print("The High Score is: {}".format(high_score))
